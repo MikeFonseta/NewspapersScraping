8af11ca0217f42ab56719796b4cfa963d062919e
MikeFonseta <seta.mik@gmail.com>
2023-02-18 19:07:04 +0100

    Final update Tripadvisor

--- Tripadvisor/main.py
+++ Tripadvisor/main.py
@@ -10,13 +10,28 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
+mesi = {'gen': '01',
+        'feb': '02',
+        'mar': '03',
+        'apr': '04',
+        'mag': '05',
+        'giu': '06',
+        'lug': '07',
+        'ago': '08',
+        'set': '09',
+        'ott': '10',
+        'nov': '11',
+        'dic': '12',}
+
+
 #Recensioni totali estratte
 tot_reviews = 0
-page = 0
-max_reviews = 10
+max_reviews = 50
 #Apertura foglio excel
 wb = Workbook()
 ws = wb.active
+
 
 #Opzioni per il webdriver
 options = EdgeOptions()
@@ -30,8 +45,9 @@ driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()),o
 
 def analisi(query="",url=""):
     global tot_reviews,wb,ws,driver,page
-
     
+    page = 0
+
     #Caricamento pagina
     driver.get(url)
     print(driver.find_element(By.CLASS_NAME, 'biGQs._P.fiohW.eIegw').text)
@@ -55,21 +71,36 @@ def analisi(query="",url=""):
 
         if(reviews is not None):
             
-            title = WebDriverWait(reviews, 10).until(EC.visibility_of_all_elements_located((By.CLASS_NAME,'biGQs._P.fiohW.qWPrE.ncFvv.fOtGX')))
-            decription = WebDriverWait(reviews, 10).until(EC.visibility_of_all_elements_located((By.CLASS_NAME,'_T.FKffI')))
-            date = WebDriverWait(reviews, 10).until(EC.visibility_of_all_elements_located((By.CLASS_NAME,'RpeCd')))
-            link = WebDriverWait(reviews, 10).until(EC.visibility_of_all_elements_located((By.CLASS_NAME,'BMQDV._F.G-.wSSLS.SwZTJ.FGwzt.ukgoS')))
+            title = WebDriverWait(reviews, 15).until(EC.visibility_of_all_elements_located((By.CLASS_NAME,'biGQs._P.fiohW.qWPrE.ncFvv.fOtGX')))
+            decription = WebDriverWait(reviews, 15).until(EC.visibility_of_all_elements_located((By.CLASS_NAME,'_T.FKffI')))
+            date = WebDriverWait(reviews, 15).until(EC.visibility_of_all_elements_located((By.CLASS_NAME,'RpeCd')))
+            link = WebDriverWait(reviews, 15).until(EC.visibility_of_all_elements_located((By.CLASS_NAME,'BMQDV._F.G-.wSSLS.SwZTJ.FGwzt.ukgoS')))
+            starsList = WebDriverWait(reviews, 15).until(EC.visibility_of_all_elements_located((By.CLASS_NAME, 'UctUV.d.H0')))
+
 
             offSet = 0
 
             for i in range(0,len(title)):
 
-                ws.append((title[i].text, decription[i].text , date[i].text[0:9], link[offSet+1].get_attribute('href')))
+                starValue = 0
+                stars = starsList[i].find_elements(By.CSS_SELECTOR, 'path')
+                for star in stars:
+                    if(star.get_attribute('d') == 'M 12 0C5.388 0 0 5.388 0 12s5.388 12 12 12 12-5.38 12-12c0-6.612-5.38-12-12-12z'):
+                        starValue += 1
+                
+                starText = ""
+                if (starValue > 1 or starValue == 0):  
+                    starText = str(starValue) + " stelle" 
+                else: 
+                    starText = str(starValue) + " stella"
+
+                ws.append((title[i].text, decription[i].text , "01/"+mesi[date[i].text[:3]]+"/"+date[i].text[4:9], link[offSet+1].get_attribute('href'), starText))
                 tot_reviews += 1
                 wb.save('Tripadvisor.xlsx')
                 offSet+=2
 
             print("Pagina:",page," | ","Recensioni trovate: " + str(tot_reviews)+" | " + "Recensioni salvate: " + str(tot_reviews))
+
 
             if(tot_reviews < max_reviews):
                 elem = driver.find_elements(By.CLASS_NAME, 'UCacc')
